9133600084a3996a22e85576362ebcc9e97ebd50
Filippov Ilya <filippov2@rg.ru>
2019-08-15 18:29:30 +0300

    Init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './App.module.sass'
+import { Button } from 'evergreen-ui'
+
+function App () {
+  return (
+    <main className={styles.app}>
+      <Button>Evergreen test</Button>
+    </main>
+  )
+}
+
+export default App
